c3197c8452b33dcf30df7ae4cfcd5cf304aadf5e
rreece15 <reece.riherd@gmail.com>
2023-04-08 20:30:55 -0500

    updated with Ludicon login
    still needs amplify/cognito integrated

--- ReactAndroid/LUDICON/App.tsx
+++ ReactAndroid/LUDICON/App.tsx
@@ -1,161 +1,103 @@
-import { StatusBar } from "expo-status-bar";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+// import { Amplify } from '@aws-amplify/core';
+// import { withAuthenticator } from 'aws-amplify-react-native';
 
-import React, { useState } from "react";
+// import { Auth } from 'aws-amplify';
+import { Linking } from 'react-native';
 
-import {
 
-  StyleSheet,
+// Amplify.configure({
+//   Auth: {
+//     region: 'us-east-2',
+//     userPoolId: 'us-east-2_zdThQOZmj',
+//     userPoolWebClientId: '4k2kvch9dl8jhs3m61ksvqbrvk',
+//     oauth: {
+//       domain: 'ludicontestdemo.auth.us-east-2.amazoncognito.com',
+//       scope: ['email', 'openid'],
+//       redirectSignIn: 'http://localhost:8000/logged_in.html',
+//       redirectSignOut: 'http://localhost:8000/logged_out.html',
+//       responseType: 'code'
+//     }
+//   }
+// });
 
-  Text,
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  View,
-
-  Image,
-
-  TextInput,
-
-  Button,
-
-  TouchableOpacity,
-
-} from "react-native";
-
-export default function App() {
-
-  const [email, setEmail] = useState("");
-
-  const [password, setPassword] = useState("");
+  const handleLogin = async () => {
+    try {
+      // const user = await Auth.signIn(username, password);
+      console.log('Username:', username);
+      console.log('Password:', password);
+      // navigate to the authenticated portion of the app
+      // Linking.openURL('http://localhost:8000/logged_in.html');
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
 
   return (
-
     <View style={styles.container}>
-
-      <StatusBar style="auto" />
-
-      <View style={styles.inputView}>
-
-        <TextInput
-
-          style={styles.TextInput}
-
-          placeholder="Email."
-
-          placeholderTextColor="#003f5c"
-
-          onChangeText={(email) => setEmail(email)}
-
-        />
-
-      </View>
-
-      <View style={styles.inputView}>
-
-        <TextInput
-
-          style={styles.TextInput}
-
-          placeholder="Password."
-
-          placeholderTextColor="#003f5c"
-
-          secureTextEntry={true}
-
-          onChangeText={(password) => setPassword(password)}
-
-        />
-
-      </View>
-
-      <TouchableOpacity>
-
-        <Text style={styles.forgot_button}>Forgot Password?</Text>
-
+        <Image source={require('./assets/logo.png')} style={styles.image} />
+      {/* <Text style={styles.title}>Ludicon</Text> */}
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
-
-      <TouchableOpacity style={styles.loginBtn}>
-
-        <Text style={styles.loginBtn}>LOGIN</Text>
-
-      </TouchableOpacity>
-
     </View>
-
   );
+};
+export default App;
+// export default withAuthenticator(App);
 
-}
 
 const styles = StyleSheet.create({
-
   container: {
-
     flex: 1,
-
-    backgroundColor: "#fff",
-
-    alignItems: "center",
-
-    justifyContent: "center",
-
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-
   image: {
-
-    marginBottom: 40,
-
-  },
-
-  inputView: {
-
-    backgroundColor: "#FFC0CB",
-
-    borderRadius: 30,
-
-    width: "70%",
-
-    height: 45,
-
+    width: 370,
+    height: 150,
     marginBottom: 20,
-
-    alignItems: "center",
-
   },
-
-  TextInput: {
-
-    height: 50,
-
-    flex: 1,
-
-    padding: 10,
-
-    marginLeft: 20,
-
-  },
-
-  forgot_button: {
-
-    height: 30,
-
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
     marginBottom: 30,
-
   },
-
-  loginBtn: {
-
-    width: "80%",
-
-    borderRadius: 25,
-
-    height: 50,
-
-    alignItems: "center",
-
-    justifyContent: "center",
-
-    marginTop: 40,
-
-    backgroundColor: "#FF1493",
-
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    width: '80%',
+    marginBottom: 20,
   },
-
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
