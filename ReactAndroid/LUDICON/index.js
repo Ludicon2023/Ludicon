cf2dc0d7a36d7bcde37b1bf7427b2da9dcb73d05
rreece15 <reece.riherd@gmail.com>
2023-04-16 21:40:56 -0500

    svc

--- ReactAndroid/LUDICON/index.js
+++ ReactAndroid/LUDICON/index.js
@@ -5,5 +5,12 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
+export default function App() {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+}
 AppRegistry.registerComponent(appName, () => App);
