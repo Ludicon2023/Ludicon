0ca2b4ba59df911b1de4b04d5747f26d2bff5723
Thibault Tonnel <ttonnel@Thibaults-MacBook-Pro.local>
2023-09-26 18:34:05 -0500

    modified joined page and renderitem component, looks much better now

--- components/RenderItem.js
+++ components/RenderItem.js
@@ -1,116 +1,73 @@
 import React from 'react';
 import { View, StyleSheet, Image, Text } from 'react-native';
 
-const RenderItem = ({ children }) => {
+const RenderItem = ({
+  imageSource,
+  distance,
+  title,
+  level,
+  eventDate,
+  address,
+  peopleCount,
+}) => {
   return (
-    <View style={[styles.tableRow, { borderColor: getRandomColor() }]}>
-      <View style={styles.topContainer}>
-        <Image source={require('../assets/zilker.png')} style={styles.image} />
-        <View style={styles.overlay}>
-          <Text style={[styles.overlayText, styles.distanceText, styles.boldText]}>0.7 Miles Away</Text>
-          <Text style={[styles.overlayText, styles.titletext, styles.bottomText]}>Frisbee in Front of the Tower</Text>
-          <View style={styles.horizontalContainer}>
-            <Text style={[styles.overlayText, styles.bottomText]}>{'Frisbee Beginner Level\n'}</Text> 
-              <Text style={[styles.overlayText, styles.bottomText]}>{'More Info>>'}</Text>
-          </View>
-        </View>
+    <View style={styles.container}>
+      <Image source={imageSource} style={styles.image} />
+      <View style={styles.textOverlay}>
+        <Text style={styles.distanceText}>{distance}</Text>
+        <Text style={styles.titleText}>{title}</Text>
+        <Text style={styles.levelText}>{level}</Text>
       </View>
-      <View style={styles.bottomContainer}>
-      <View style={[styles.bottomTextContainer, { borderRightWidth: 1 }]}>
-          <Text style={styles.bottomSectionText}>Monday, April 17 at 5:00pm</Text>
-        </View>
-        <View style={[styles.bottomTextContainer, { borderRightWidth: 1 }]}>
-          <Text style={styles.bottomSectionText}>Address of the event</Text>
-        </View>
-        <View style={[styles.bottomTextContainer, { borderRightWidth: 1 }]}>
-          <Text style={styles.bottomSectionText}>Number of people in current group</Text>
-        </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoText}>{eventDate}</Text>
+        <Text style={styles.infoText}>{address}</Text>
+        <Text style={styles.infoText}>{peopleCount}</Text>
       </View>
     </View>
   );
 };
 
-const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
 const styles = StyleSheet.create({
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'space-between',
-    padding: 16,
-    borderColor: 'black',
-    borderWidth: 3,
-    borderRadius: 16,
-  },
-  overlayText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold', 
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  distanceText: {
-    fontWeight: 'bold',
-    textAlign: 'right',
-  },
-  horizontalContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'flex-end',
-  },
-  rightContainer: {
-    alignItems: 'flex-end',
-  },
-  topContainer: {
-    flex: 0.7,
-    position: 'relative',
-    width: '100%',
-    aspectRatio: 1.5,
-  },
-  bottomContainer: {
-    flex: 0.3,
-    width: '100%',
-    backgroundColor: 'white',
+  container: {
     flexDirection: 'column',
+    backgroundColor: '#E5E5E5',
+    borderRadius: 10,
+    overflow: 'hidden',
   },
   image: {
     width: '100%',
-    height: '100%',
+    height: 150,
     resizeMode: 'cover',
   },
-  tableRow: {
-    height: 207,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10,
-    borderWidth:3,
+  textOverlay: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    right: 10,
   },
-  titletext:{
-    fontSize: 26,
-  },
-  tableText: {
-    fontSize: 24,
+  distanceText: {
     color: 'white',
     fontWeight: 'bold',
+    textAlign: 'right',
   },
-  bottomTextContainer: {
-    borderStyle: 'solid',
-    padding: 1,
+  titleText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
   },
-  bottomSectionText: {
-    fontSize: 13,
-    color: 'blak',
+  levelText: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
+  infoContainer: {
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',  // Slight transparency to separate from image
+  },
+  infoText: {
+    color: 'black',
     fontWeight: 'bold',
   },
 });
--- pages/Joined.js
+++ pages/Joined.js
@@ -1,42 +1,87 @@
-import React from 'react';
-import { View, Text, StyleSheet, FlatList, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import RenderItem from '../components/RenderItem';
 
 const Joined = () => {
-  const tableData = new Array(10).fill({});
-  const tableItemHeight = 150;
+  const tableData = [
+    {
+      imageSource: require('../assets/zilker.png'),
+      distance: "5km",
+      title: "Basketball Game",
+      level: "Intermediate",
+      eventDate: "26th September",
+      address: "Park Lane 123",
+      peopleCount: "5/10",
+    },
+    {
+      imageSource: require('../assets/zilker.png'),
+      distance: "5km",
+      title: "Basketball Game",
+      level: "Intermediate",
+      eventDate: "26th September",
+      address: "Park Lane 123",
+      peopleCount: "5/10",
+    },
+    {
+      imageSource: require('../assets/zilker.png'),
+      distance: "5km",
+      title: "Basketball Game",
+      level: "Intermediate",
+      eventDate: "26th September",
+      address: "Park Lane 123",
+      peopleCount: "5/10",
+    },
+    {
+      imageSource: require('../assets/zilker.png'),
+      distance: "5km",
+      title: "Basketball Game",
+      level: "Intermediate",
+      eventDate: "26th September",
+      address: "Park Lane 123",
+      peopleCount: "5/10",
+    },
+    {
+      imageSource: require('../assets/zilker.png'),
+      distance: "5km",
+      title: "Basketball Game",
+      level: "Intermediate",
+      eventDate: "26th September",
+      address: "Park Lane 123",
+      peopleCount: "5/10",
+    }
+    
+  ];
+
+  const tableItemHeight = 207;
 
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={[styles.title, styles.titleContent]}>My Joined Games</Text>
-        <Text style={[styles.title, styles.titleContent]}>(3)</Text>
+        <Text style={[styles.title, styles.titleContent]}>+</Text>
       </View>
       <View style={styles.content}>
-        <ScrollView contentContainerStyle={{ paddingHorizontal: 25, paddingTop: 25 }}showsVerticalScrollIndicator={false}>
-          <View style={{ height: 500 }}>
-            <FlatList
-              data={tableData}
-              renderItem={RenderItem}
-              ItemSeparatorComponent={() => <View style={{ height: 25 }} />}
-              getItemLayout={(_, index) => ({
-                length: '100%',
-                offset: (tableItemHeight + 25) * index,
-                index,
-              })}
-            />
-          </View>
-        </ScrollView>
+        <FlatList
+          data={tableData}
+          renderItem={({ item }) => <RenderItem {...item} />}
+          ItemSeparatorComponent={() => <View style={{ height: 25 }} />}
+          getItemLayout={(_, index) => ({
+            length: tableItemHeight,
+            offset: (tableItemHeight + 25) * index,
+            index,
+          })}
+          contentContainerStyle={{ paddingHorizontal: 25, paddingTop: 25, paddingBottom: 100 }}
+          showsVerticalScrollIndicator={false}
+        />
       </View>
     </View>
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }, 
+  },
   titleContainer: {
     flex: 0.12,
     flexDirection: 'row',
@@ -44,24 +89,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingLeft: 10,
     paddingRight: 10,
-    paddingtop: 10,
+    paddingTop: 10, 
     backgroundColor: '#AAFFA7',
   },
   titleContent: {
     top: 20,
   },
   content: {
-      flex: 0.88,
-      borderTopWidth: 3,
-      borderColor: 'black',
-      backgroundColor: 'white',
+    flex: 0.88,
+    borderTopWidth: 3,
+    borderColor: 'black',
+    backgroundColor: 'white',
   },
-
   title: {
     fontSize: 24,
     fontWeight: 'bold',
   },
-  
 });
 
 export default Joined;
