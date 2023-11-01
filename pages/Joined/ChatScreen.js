65f3a4cfec7388a6dba7b8aa66d884b8dbebf4d4
David Gross <78630654+davidgross631@users.noreply.github.com>
2023-11-01 14:46:20 -0500

    Update ChatScreen.js for Firebase Config
    Now able to return the first message of a collection of a particular event

--- pages/Joined/ChatScreen.js
+++ pages/Joined/ChatScreen.js
@@ -1,46 +1,88 @@
-import React, { useEffect, useState } from "react";
-import { ScrollView, Image, View, TouchableOpacity} from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, View, TouchableOpacity } from "react-native";
 import { useUser } from "../../contexts/UserContext";
-import {Text, Layout, ListItem, Button, Icon} from "@ui-kitten/components";
+import { Text, Layout, Icon } from "@ui-kitten/components";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5SCmPDmgj1_mXaMCanGe-zu5E7lKa6Ac",
+  authDomain: "ludicon-e292d.firebaseapp.com",
+  projectId: "ludicon-e292d",
+  storageBucket: "ludicon-e292d.appspot.com",
+  messagingSenderId: "1026436891213",
+  appId: "1:1026436891213:web:e8f3499b66578dadc7d204",
+  measurementId: "G-7XPQY8SNQL"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const ChatScreen = ({ route, navigation }) => {
   const { event } = route.params;
-  const {user} = useUser();
+  const { user } = useUser();
+  const [chatMessages, setChatMessages] = useState([]);
+
   useEffect(() => {
     if (user) {
-      console.log("Welcome to Chats, " + user.attributes.name); 
-      console.log("This is Event: " + event.ID); 
+      console.log("Welcome to Chats, " + user.attributes.name);
+      console.log("This is Event: " + event.ID);
+
+      // Query Firestore to find the document with the matching event.ID
+      const chatCollectionRef = collection(db, event.ID);
+
+      getDocs(chatCollectionRef)
+        .then((querySnapshot) => {
+          const messages = [];
+          querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            messages.push({
+              displayedUser: data.displayedUser,
+              message: data.message,
+            });
+          });
+          setChatMessages(messages);
+        })
+        .catch((error) => {
+          console.error("Error getting chat messages: ", error);
+        });
     }
-  }, [user]);
+  }, [user, event.ID]);
 
   return (
     <ScrollView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
-    {/* HEADER */}
-    <Layout
-      style={{
-        padding: 10,
-        paddingTop: 36,
-        backgroundColor: "#AAFFA7",
-        borderRadius: 8,
-      }}
-    >
-      <View
+      {/* HEADER */}
+      <Layout
         style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
+          padding: 10,
+          paddingTop: 36,
+          backgroundColor: "#AAFFA7",
+          borderRadius: 8,
         }}
       >
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Icon name="arrow-back-outline" width={32} height={32} />
-        </TouchableOpacity>
-        <Text category="h4">Chat: {event.Name}</Text>
-      </View>
-    </Layout>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Icon name="arrow-back-outline" width={32} height={32} />
+          </TouchableOpacity>
+          <Text category="h4">Chat: {event.Name}</Text>
+        </View>
+      </Layout>
 
-    {/* code here */}
-
-
-  </ScrollView>
+{/* Display chat messages */
+chatMessages.map((message, index) => (
+  <View key={index}>
+    <Text>{message.displayedUser}</Text>
+    <Text>{message.message}</Text>
+  </View>
+))}
+    </ScrollView>
   );
 };
 
