5e6eba806087241da440658988a69f380bffb85b
David Gross <78630654+davidgross631@users.noreply.github.com>
2023-11-01 15:06:15 -0500

    Update ChatScreen.js
    Imports the Individual Message Blueprint

--- pages/Joined/ChatScreen.js
+++ pages/Joined/ChatScreen.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { ScrollView, View, TouchableOpacity } from "react-native";
+import { ScrollView, View, TouchableOpacity, TextInput, Button } from "react-native";
 import { useUser } from "../../contexts/UserContext";
 import { Text, Layout, Icon } from "@ui-kitten/components";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc, onSnapshot } from "firebase/firestore"; // Import onSnapshot
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import IndividualMessageBlueprint from './IndividualMessageBlueprint';
 
 const firebaseConfig = {
   apiKey: "AIzaSyC5SCmPDmgj1_mXaMCanGe-zu5E7lKa6Ac",
@@ -23,30 +24,49 @@ const ChatScreen = ({ route, navigation }) => {
   const { event } = route.params;
   const { user } = useUser();
   const [chatMessages, setChatMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState(""); // State to hold the user's input
+
+  const handleSendMessage = async () => {
+    if (user && messageInput) {
+      const chatCollectionRef = collection(db, event.ID);
+
+      // Add a new document with "message," "displayedUser," and "hiddenUser" fields
+      await addDoc(chatCollectionRef, {
+        message: messageInput,
+        displayedUser: user.attributes.name,
+        hiddenUser: user.attributes.email,
+      });
+
+      setMessageInput(""); // Clear the input field
+    }
+  };
 
   useEffect(() => {
     if (user) {
       console.log("Welcome to Chats, " + user.attributes.name);
+      console.log("Email for user is. " + user.attributes.email);
       console.log("This is Event: " + event.ID);
 
-      // Query Firestore to find the document with the matching event.ID
+      // Query Firestore to find the documents in the collection
       const chatCollectionRef = collection(db, event.ID);
 
-      getDocs(chatCollectionRef)
-        .then((querySnapshot) => {
-          const messages = [];
-          querySnapshot.forEach((doc) => {
-            const data = doc.data();
-            messages.push({
-              displayedUser: data.displayedUser,
-              message: data.message,
-            });
+      // Listen for changes to the collection and update the messages
+      const unsubscribe = onSnapshot(chatCollectionRef, (querySnapshot) => {
+        const messages = [];
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          messages.push({
+            displayedUser: data.displayedUser,
+            message: data.message,
           });
-          setChatMessages(messages);
-        })
-        .catch((error) => {
-          console.error("Error getting chat messages: ", error);
         });
+        setChatMessages(messages);
+      });
+
+      // Clean up the listener when the component is unmounted
+      return () => {
+        unsubscribe();
+      };
     }
   }, [user, event.ID]);
 
@@ -77,11 +97,23 @@ const ChatScreen = ({ route, navigation }) => {
 
 {/* Display chat messages */
 chatMessages.map((message, index) => (
-  <View key={index}>
-    <Text>{message.displayedUser}</Text>
-    <Text>{message.message}</Text>
-  </View>
+  <IndividualMessageBlueprint
+    key={index}
+    displayedUser={message.displayedUser}
+    message={message.message}
+  />
 ))}
+
+
+      {/* Input field for user messages */}
+      <View>
+        <TextInput
+          placeholder="Type your message here"
+          value={messageInput}
+          onChangeText={(text) => setMessageInput(text)}
+        />
+        <Button title="Send" onPress={handleSendMessage} />
+      </View>
     </ScrollView>
   );
 };
